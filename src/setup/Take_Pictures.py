57f9f7463e28683e59f51742c3ab4342cb63a816
ntczkjfg <ntczkjfg@protonmail.com>
2024-11-08 23:13:19 -0500

    Updated Take_Pictures
    Uses pathlib, and checks for errors while operating

--- src/setup/Take_Pictures.py
+++ src/setup/Take_Pictures.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+from time import sleep
+from io import BytesIO
+
+import numpy as np
 import board
 import neopixel
 from picamera import PiCamera
-import os
-from time import time, sleep
 
 LED_COUNT = 1200
 LED_PIN = board.D18 # Pin 12 (Use 14 for ground)
@@ -15,14 +18,41 @@ camera.resolution = (601, 1001)
 ON = (0, 0, 255) # Blue
 OFF = (0, 0, 0)
 
-PATH = "/home/pi/Desktop/TreeLights/TreePhotos/"
+path = Path('/home/pi/Desktop/TreeLights/TreePhotos/')
 
-def next(i, j):
+def light_up(i, sleep_dur = 0.5):
     pixels.fill(OFF)
     pixels[i] = ON
     pixels.show()
-    pixels.show()
-    print(f'{j}/{("00"+str(i))[-3:]}', end = ', ')
+    sleep(sleep_dur)
+    max_retries = 5
+    for _ in range(max_retries):
+        if check_for_error():
+            pixels.show()
+            sleep(sleep_dur)
+        else:
+            break
+
+def check_for_error():
+    stream = BytesIO()
+    camera.capture(stream, format='jpeg')
+    stream.seek(0)
+    image = Image.open(stream)
+    image = np.array(image)
+    r = image[:, :, 0]
+    g = image[:, :, 1]
+    b = image[:, :, 2]
+    threshold_high = 50
+    threshold_low = 10
+    red_pixels = (r > threshold_high) & (g < threshold_low) & (b < threshold_low)
+    green_pixels = (g > threshold_high) & (r < threshold_low) & (b < threshold_low)
+    if red_pixels.any() or green_pixels.any():
+        return True
+    return False
+
+def save_photo(i, j):
+    save_path = path / str(j) / ('00' + str(i))[-3:] + '.jpeg'
+    camera.capture(save_path, format='jpeg')
 
 def calibrate(dark = False):
     if dark:
@@ -31,16 +61,10 @@ def calibrate(dark = False):
         pixels.fill(ON)
     pixels.show()
     camera.start_preview(alpha = 230)
-    input("Press enter to end calibration...")
+    input('Press enter to end calibration...')
     pixels.fill(OFF)
     pixels.show()
     camera.stop_preview()
-
-def takePicture(i, j, save):
-    next(i, j)
-    sleep(.5)
-    if save:
-        camera.capture(PATH + str(j) + "/" + (str("00") + str(i))[-3:] + ".jpg")
 
 # Takes a picture of each LED in sequence.
 # Does that 8 times in a row, for 8 different angles.
@@ -48,58 +72,57 @@ def takePicture(i, j, save):
 # In manual mode, pressing Enter takes a picture, while entering
 # an apostrophe re-sends the light commands, to control for errors.
 # Enter q in manual mode to change to automatic mode.  
-def takePictures(savePhotos = False, manual = False, preview = True):
-    if savePhotos == False:
-        print("Not saving photos")
+def take_pictures(save_photos = False, manual = False, preview = True):
+    if save_photos == False:
+        print('Not saving photos')
     else:
-        print("Saving photos")
+        print('Saving photos')
     if manual:
-        print("Manual controls enabled")
+        print('Manual controls enabled')
     try:
         if preview:
             camera.start_preview(alpha = 230)
         for j in range(1, 9):
             if savePhotos:
-                os.mkdir(PATH + str(j))
+                path_j = path / str(j)
+                path_j.mkdir(parents = True)
             for i in range(LED_COUNT):
                 if manual:
                     x = "'"
                     while x == "'":
-                        next(i, j)
-                        if x == "q":
+                        light_up(i, sleep_dur = 0)
+                        if x == 'q':
                             manual = False
                             break
                 else:
-                    next(i, j)
-                    sleep(.5)
-                if savePhotos:
-                    camera.capture(PATH + str(j) + "/" + (str("00") + str(i))[-3:] + ".jpg")
+                    light_up(i)
+                if save_photos:
+                    save_photo(i, j)
                 sleep(0.1)
             print()
             if j < 8:
-                x = input("Rotate the tree 45° clockwise and press enter...")
+                x = input('Rotate the tree 45° clockwise and press enter...')
             else:
-                print("Done.")
+                print('Done.')
     finally:
-        camera.stop_preview()
+        if preview:
+            camera.stop_preview()
 
 # Retakes only specified pictures, for fixing errors in mass.
 # Enter errors into "errors" variable as [j, i] lists from the above function.
 # Press enter to take picture, or enter an apostrophe if an error is still present to re-send light commands
-def fixErrors():
-    camera.start_preview(alpha = 230)
-    errors = []
-    for error in errors:
-        j = error[0]
-        i = error[1]
-        x = "'"
-        try:
+def fix_errors():
+    try:
+        camera.start_preview(alpha = 230)
+        errors = []
+        for error in errors:
+            j = error[0]
+            i = error[1]
+            x = "'"
             while x == "'":
-                next(i, j)
-                x = input("...")
-            camera.capture(PATH + str(j) + "/" + (str("00") + str(i))[-3:] + ".jpg")
+                light_up(i, sleep_dur = 0)
+                x = input("Press Enter to take picture, or ' to retry the the light")
+            save_photo(i, j)
             sleep(0.1)
-        except KeyboardInterrupt:
-            camera.stop_preview()
-            return
-    camera.stop_preview()
+    finally:
+        camera.stop_preview()
