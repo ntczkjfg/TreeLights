bb8c75088896739db7e3d909b1ddebe5f635f2dd
ntczkjfg <ntczkjfg@protonmail.com>
2025-01-05 06:07:01 -0500

    Removed start_tree_lights.sh

--- src/start_tree_lights.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DISPLAY=:0
-while ! xset q &>/dev/null; do
-	sleep 1
-done
-cd /home/user/Desktop/TreeLights/src/
-lxterminal -e "sudo /usr/bin/python3 /home/user/Desktop/TreeLights/src/effect_control.py"
